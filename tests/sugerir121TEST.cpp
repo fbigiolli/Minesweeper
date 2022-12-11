151169e7c49760549d33beca2d38d5433517f063
fbigiolli <felipebigiolli@gmail.com>
2022-12-11 16:05:27 -0300

    minesweeper v1

--- tests/sugerir121TEST.cpp
+++ tests/sugerir121TEST.cpp
@@ -1,190 +1,118 @@
-// Tests de la catedra para la función sugerirAutomatico121.
+// Tests nuestros para la función sugerir121.
 // Ver el archovo README.md para ayuda
 
 #include "../ejercicios.h"
 #include "../auxiliares.h"
 #include "gtest/gtest.h"
-#include "../definiciones.h"
 
 using namespace std;
 
-// ┌───┬───┬───┬───┬───┐
-// │ * │ * │ 1 │ 0 │ 0 │
-// ├───┼───┼───┼───┼───┤
-// │ 3 │ 3 │ 2 │ 0 │ 0 │
-// ├───┼───┼───┼───┼───┤
-// │ 1 │ * │ 1 │ 0 │ 0 │
-// ├───┼───┼───┼───┼───┤
-// │ 2 │ 3 │ 2 │ 1 │ 0 │
-// ├───┼───┼───┼───┼───┤
-// │ * │ 2 │ * │ 1 │ 0 │
-// └───┴───┴───┴───┴───┘
-tablero t_vert = {
+tablero t5x5 = {
         { cMINA,  cMINA,  cVACIA, cVACIA, cVACIA },
+        { cVACIA, cVACIA, cVACIA, cMINA,  cVACIA },
         { cVACIA, cVACIA, cVACIA, cVACIA, cVACIA },
-        { cVACIA, cMINA,  cVACIA, cVACIA, cVACIA },
-        { cVACIA, cVACIA, cVACIA, cVACIA, cVACIA },
-        { cMINA,  cVACIA, cMINA,  cVACIA, cVACIA },
+        { cVACIA, cVACIA, cVACIA, cMINA, cVACIA },
+        { cMINA,  cVACIA, cVACIA,  cVACIA, cVACIA },
 };
 
-// ┌───┬───┬───┬───┬───┐
-// │ 1 │ * │ 2 │ * │ 1 │
-// ├───┼───┼───┼───┼───┤
-// │ 2 │ 2 │ 4 │ 2 │ 2 │
-// ├───┼───┼───┼───┼───┤
-// │ 1 │ * │ 2 │ * │ 1 │
-// ├───┼───┼───┼───┼───┤
-// │ 2 │ 3 │ 3 │ 2 │ 1 │
-// ├───┼───┼───┼───┼───┤
-// │ * │ 2 │ * │ 1 │ 0 │
-// └───┴───┴───┴───┴───┘
-tablero t_borde = {
-        { cVACIA, cMINA,  cVACIA, cMINA,  cVACIA },
+tablero t121Borde = {
+        { cVACIA,  cVACIA,  cVACIA, cVACIA, cVACIA },
+        { cVACIA, cMINA, cVACIA, cMINA,  cVACIA },
         { cVACIA, cVACIA, cVACIA, cVACIA, cVACIA },
-        { cVACIA, cMINA,  cVACIA, cMINA,  cVACIA },
-        { cVACIA, cVACIA, cVACIA, cVACIA, cVACIA },
-        { cMINA,  cVACIA, cMINA,  cVACIA, cVACIA },
+        { cVACIA, cVACIA, cVACIA, cMINA, cVACIA },
+        { cMINA,  cVACIA, cVACIA,  cVACIA, cVACIA },
 };
 
-// ┌───┬───┬───┬───┬───┐
-// │ * │ * │ 1 │ 0 │ 0 │
-// ├───┼───┼───┼───┼───┤
-// │ 2 │ 2 │ 1 │ 1 │ 1 │
-// ├───┼───┼───┼───┼───┤
-// │ 1 │ 1 │ 0 │ 1 │ * │
-// ├───┼───┼───┼───┼───┤
-// │ * │ 1 │ 1 │ 2 │ 1 │
-// ├───┼───┼───┼───┼───┤
-// │ 1 │ 2 │ * │ 1 │ 0 │
-// └───┴───┴───┴───┴───┘
-tablero t_hor = {
-        { cMINA,  cMINA,  cVACIA, cVACIA, cVACIA },
-        { cVACIA, cVACIA, cVACIA, cVACIA, cVACIA },
-        { cVACIA, cVACIA, cVACIA, cVACIA, cMINA  },
-        { cMINA,  cVACIA, cVACIA, cVACIA, cVACIA },
-        { cVACIA, cVACIA, cMINA,  cVACIA, cVACIA },
+tablero t4 = {
+        { cVACIA,  cVACIA,  cVACIA, cVACIA, cMINA, cVACIA, cMINA, cVACIA, cVACIA, cMINA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cMINA, cVACIA, cVACIA, cVACIA },
+        { cVACIA,  cVACIA,  cVACIA, cMINA, cVACIA, cMINA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cMINA, cVACIA, cMINA, cMINA},
+        { cVACIA,  cVACIA,  cVACIA, cVACIA, cVACIA, cMINA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA },
+        { cVACIA,  cMINA,  cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA },
+        { cVACIA,  cVACIA,  cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cMINA, cVACIA },
+        { cVACIA,  cVACIA,  cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cMINA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA },
+        { cVACIA,  cVACIA,  cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cMINA, cVACIA, cVACIA, cVACIA, cVACIA, cMINA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA },
+        { cVACIA,  cVACIA,  cVACIA, cMINA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA },
+        { cVACIA,  cVACIA,  cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cMINA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cMINA, cVACIA },
+        { cVACIA,  cVACIA,  cVACIA, cMINA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cMINA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA },
+        { cVACIA,  cVACIA,  cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA },
+        { cMINA,  cVACIA,  cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cMINA, cVACIA, cVACIA, cVACIA, cMINA, cMINA },
+        { cVACIA,  cVACIA,  cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cMINA, cVACIA, cVACIA, cMINA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cMINA },
+        { cVACIA,  cVACIA,  cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cMINA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA },
+        { cMINA,  cMINA,  cVACIA, cVACIA, cVACIA, cMINA, cVACIA, cVACIA, cMINA, cVACIA, cVACIA, cMINA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA },
+        { cVACIA,  cVACIA,  cMINA, cVACIA, cVACIA, cVACIA, cMINA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cMINA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA },
+        { cVACIA,  cMINA,  cVACIA, cVACIA, cVACIA, cVACIA, cMINA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cMINA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA },
+        { cVACIA,  cVACIA,  cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cMINA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA },
+        { cVACIA,  cVACIA,  cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA, cVACIA },
+        { cMINA,  cMINA,  cMINA, cMINA, cVACIA, cVACIA, cVACIA, cVACIA, cMINA, cVACIA, cVACIA, cVACIA, cVACIA, cMINA, cVACIA, cMINA, cVACIA, cMINA, cMINA, cVACIA },
+
 };
 
-// ┌───┬───┐
-// │ 0 │ 0 │
-// ├───┼───┤
-// │ 0 │ 0 │
-// └───┴───┘
-tablero t_nohay121 = {
-        { cVACIA, cVACIA },
-        { cVACIA, cVACIA }
-};
-
-TEST(sugerirAutomatico121TEST, sugerenciaValida121Vertical){
-
-    jugadas j = {
-            jugada(pos(0, 2), 1), jugada(pos(1, 2), 2), jugada(pos(2, 2), 1), // configuracion 121 vertical
-            jugada(pos(1, 4), 0),
-            jugada(pos(0, 3), 0), jugada(pos(2, 0), 1),
-            jugada(pos(4, 1), 2), jugada(pos(4, 3), 1), jugada(pos(4, 4), 0),
-    };
-
-    pos exp_1 = pos(1, 1);
-    pos exp_2 = pos(1, 3);
-    banderitas b = {
-            pos(0, 0),
-            pos(0, 1),
-    };
-
-    pos sugerido = pos(-1, -1);
-    bool resp = sugerirAutomatico121(t_vert, b, j, sugerido);
-
-    EXPECT_TRUE(resp && (sugerido == exp_1 || sugerido == exp_2));
+TEST(sugerirAutomatico121, testHorizontalArribaBorde) {
+    jugadas j = {jugada(pos(0, 1), 1), jugada(pos(0, 2), 2), jugada(pos(0, 3), 1)};
+    tablero t = t121Borde;
+    banderitas b = {{2,1},{2,3}};
+    pos p = {};
+    pos expected = {1,2};
+    bool result = sugerirAutomatico121 (t,b,j,p);
+    ASSERT_EQ(result, true);
+    ASSERT_EQ(p,expected);
 }
 
-TEST(sugerirAutomatico121TEST, sugerenciaValida121Horizontal){
-
-    jugadas j = {
-            jugada(pos(3, 2), 1), jugada(pos(3, 3), 2), jugada(pos(3, 4), 1), // configuracion 121 horizontal
-            jugada(pos(1, 4), 1),
-    };
-
-    pos exp_1 = pos(2, 3);
-    pos exp_2 = pos(4, 3);
-    banderitas b = {
-            pos(0, 0),
-            pos(0, 1),
-    };
-
-    pos sugerido = pos(-1, -1);
-    bool resp = sugerirAutomatico121(t_hor, b, j, sugerido);
-
-    EXPECT_TRUE(resp && (sugerido == exp_1 || sugerido == exp_2));
-}
-
-TEST(sugerirAutomatico121TEST, sugerenciaValida121HorizontalConBanderitaSuperpuesta){
-
-    jugadas j = {
-            jugada(pos(3, 2), 1), jugada(pos(3, 3), 2), jugada(pos(3, 4), 1), // configuracion 121 horizontal
-            jugada(pos(1, 4), 1),
-    };
-
-    pos exp_1 = pos(2, 3);
-    banderitas b = {
-            pos(0, 0),
-            pos(4, 3),
-    };
-
-    pos sugerido = pos(-1, -1);
-    bool resp = sugerirAutomatico121(t_hor, b, j, sugerido);
-
-    EXPECT_TRUE(resp && sugerido == exp_1);
-}
-
-TEST(sugerirAutomatico121TEST, sugerenciaValida121VerticalBorde){
-
-    jugadas j = {
-            jugada(pos(0, 4), 1), jugada(pos(1, 4), 2), jugada(pos(2, 4), 1), // configuracion 121 vertical
-            jugada(pos(0, 3), 0), jugada(pos(2, 0), 1),
-            jugada(pos(4, 1), 2),
-    };
-
-    pos exp_1 = pos(1, 3);
-    banderitas b = {
-            pos(0, 0),
-            pos(0, 1),
-    };
-
-    pos sugerido = pos(-1, -1);
-    bool resp = sugerirAutomatico121(t_borde, b, j, sugerido);
-
-    EXPECT_TRUE(resp && sugerido == exp_1);
-}
-
-TEST(sugerirAutomatico121TEST, sugerencia2Validas121VerticalBordeConBanderita){
-
-    jugadas j = {
-            jugada(pos(0, 4), 1), jugada(pos(1, 4), 2), jugada(pos(2, 4), 1), // configuracion 121 vertical
-            jugada(pos(0, 0), 1),  jugada(pos(1, 0), 2), jugada(pos(2, 0), 1), // configuracion 121 vertical pero tiene banderita
-            jugada(pos(4, 1), 2),
-    };
-
-    pos exp_1 = pos(1, 3);
-    banderitas b = {
-            pos(1, 1),
-    };
-
-    pos sugerido = pos(-1, -1);
-    bool resp = sugerirAutomatico121(t_borde, b, j, sugerido);
-
-    EXPECT_TRUE(resp && sugerido == exp_1);
-}
-
-TEST(sugerirAutomatico121TEST, noHaySugerenciaPosible){
-
-    jugadas j = {
-            jugada(pos(0, 0), 0)
-    };
-
+TEST(sugerirAutomatico121, testNormal) {
+    jugadas j = {jugada(pos(15, 9), 1), jugada(pos(15, 10), 1), jugada(pos(15, 11), 1), jugada(pos(15, 12), 2),
+                 jugada(pos(16, 9), 1), jugada(pos(16, 10), 0), jugada(pos(16, 11), 0), jugada(pos(16, 12), 1), jugada(pos(16, 13), 2),
+                 jugada(pos(17, 9), 1), jugada(pos(17, 10), 0), jugada(pos(17, 11), 0), jugada(pos(17, 12), 0), jugada(pos(17, 13), 1),
+                 jugada(pos(18, 9), 2), jugada(pos(18, 10), 0), jugada(pos(18, 11), 0), jugada(pos(18, 12), 1), jugada(pos(18, 13), 1),
+                 jugada(pos(19, 9), 1), jugada(pos(19, 10), 0), jugada(pos(19, 11), 0), jugada(pos(19, 12), 1)};;
+    tablero t = t4;
     banderitas b = {};
+    pos p = {};
+    pos expected = {18,8};
+    bool result = sugerirAutomatico121 (t,b,j,p);
+    ASSERT_EQ(result, true);
+    ASSERT_EQ(p,expected);
+}
 
-    pos sugerido = pos(-1, -1);
-    bool resp = sugerirAutomatico121(t_borde, b, j, sugerido);
+//mismo test que arriba solo que modifique la posicion donde habia 121 para que no tenga.
+TEST(sugerirAutomatico121, testBanderitasEnLosCandidatosAJugar) {
+    jugadas j = {jugada(pos(1, 2), 1), jugada(pos(2, 2), 2), jugada(pos(3, 2), 1)};
+    tablero t = t5x5;
+    banderitas b = {{2,1},{2,3}};
+    pos p = {};
+    pos expected = {};
+    bool result = sugerirAutomatico121 (t,b,j,p);
+    ASSERT_EQ(result, false);
+    ASSERT_EQ(p,expected);
+}
 
-    EXPECT_FALSE(resp); // no hay 121 -> no hay sugerencia
+
+//igual al test1 pero donde tendria que estar la sugerencia hay una banderita.
+TEST(sugerirAutomatico121, testNormalPeroConBanderita) {
+    jugadas j = {jugada(pos(15, 9), 1), jugada(pos(15, 10), 1), jugada(pos(15, 11), 1), jugada(pos(15, 12), 2),
+                 jugada(pos(16, 9), 1), jugada(pos(16, 10), 0), jugada(pos(16, 11), 0), jugada(pos(16, 12), 1), jugada(pos(16, 13), 2),
+                 jugada(pos(17, 9), 1), jugada(pos(17, 10), 0), jugada(pos(17, 11), 0), jugada(pos(17, 12), 0), jugada(pos(17, 13), 1),
+                 jugada(pos(18, 9), 2), jugada(pos(18, 10), 0), jugada(pos(18, 11), 0), jugada(pos(18, 12), 1), jugada(pos(18, 13), 1),
+                 jugada(pos(19, 9), 1), jugada(pos(19, 10), 0), jugada(pos(19, 11), 0), jugada(pos(19, 12), 1)};;
+    tablero t = t4;
+    banderitas b = {{18, 8}};
+    pos p = {};
+    pos expected = {};
+    bool result = sugerirAutomatico121 (t,b,j,p);
+    ASSERT_EQ(result, false);
+    ASSERT_EQ(p,expected);
+}
+
+TEST(sugerirAutomatico121, testJugadasALosCostados) {
+    jugadas j = {jugada(pos(15, 9), 1), jugada(pos(15, 10), 1), jugada(pos(15, 11), 1), jugada(pos(15, 12), 2),
+                 jugada(pos(16, 9), 1), jugada(pos(16, 10), 0), jugada(pos(16, 11), 0), jugada(pos(16, 12), 1), jugada(pos(16, 13), 2),
+                 jugada(pos(17, 9), 1), jugada(pos(17, 10), 0), jugada(pos(17, 11), 0), jugada(pos(17, 12), 0), jugada(pos(17, 13), 1),
+                 jugada(pos(18, 9), 2), jugada(pos(18, 10), 0), jugada(pos(18, 11), 0), jugada(pos(18, 12), 1), jugada(pos(18, 13), 1),
+                 jugada(pos(19, 9), 1), jugada(pos(19, 10), 0), jugada(pos(19, 11), 0), jugada(pos(19, 12), 1), jugada(pos(18, 8), 2) };;
+    tablero t = t4;
+    banderitas b = {};
+    pos p = {};
+    pos expected = {};
+    bool result = sugerirAutomatico121 (t,b,j,p);
+    ASSERT_EQ(result, false);
+    ASSERT_EQ(p,expected);
 }
